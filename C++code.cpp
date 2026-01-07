0fac9d3dab4a055faea5a12514fc1db75933052a
khan-Momna <momnak027@gmail.com>
2026-01-06 20:36:19 -0800

    Add files via upload

---
+++ C++code.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // Prints "Hello, World!" to the console
+    std::cout << "Hello, World!" << std::endl;
+    
+    // Returns 0 to indicate successful execution
+    return 0;
+// Prints "Heelo to sky" to the console
+}
